bb0cc204b4f9c5ab9e387ac476df543175dfdf15
Ilyas Hirsi <ilyashirsi21@gmail.com>
2024-10-03 19:16:18 -0400

    pygame

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-print("hello world - ariel")
-print("yo")
-for i in range (5):
-    print("matthew lee",+i)
+import pygame
+pygame.init()
 
-print("trial")
+WIDTH, HEIGHT = 500, 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+RED = (255, 0, 0)
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    screen.fill(RED)
+    pygame.display.update()
+    
