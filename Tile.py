97c14a36a45126a8b4f5a8e7155c463f817985b7
Ilyas Hirsi <ilyashirsi21@gmail.com>
2024-10-04 21:02:39 -0400

    another velocity change

--- Tile.py
+++ Tile.py
@@ -6,6 +6,7 @@ class Tile:
         self.pos = pygame.math.Vector2(grid_pos[0] * SCALE, grid_pos[1] * SCALE)
         self.color = color
         self.hit_box = pygame.Rect(self.pos.x, self.pos.y, SCALE, SCALE)
+        
 
     def draw(self, offset):
         pygame.draw.rect(self.display, self.color, (self.pos.x-offset.x, self.pos.y-offset.y, SCALE, SCALE))
